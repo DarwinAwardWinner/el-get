a70c3a697a5aa7cb89d0ab46559f7ee05bc82bb6
Ryan C. Thompson <rct@thompsonclan.org>
2013-05-29 23:33:42 -0700

    Fix test 613 to work around a bug in loadhist

--- test/el-get-issue-613.el
+++ test/el-get-issue-613.el
@@ -13,10 +13,10 @@
         "n3-mode package should not be loaded because el-get is lazy")
 (assert (functionp 'n3-mode) nil
         "n3-mode function should be defined because it is autoloaded")
-(assert (equal 'autoload (car (symbol-function 'n3-mode))) nil
+(assert (equal 'autoload (car-safe (symbol-function 'n3-mode))) nil
         "n3-mode function definition should be an autoload")
 (with-temp-buffer (n3-mode))
-(assert (file-loadhist-lookup "n3-mode") nil
+(assert (functionp 'n3-comment-dwim) nil
         "n3-mode package should be loaded after calling n3-mode function")
 (assert (not (equal 'autoload (car-safe (symbol-function 'n3-mode)))) nil
         "n3-mode function should no longer be an autoload after calling it")
