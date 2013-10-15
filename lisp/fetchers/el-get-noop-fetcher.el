1f11e6286f850470aa93a93dc29cafa9c0112b1d
Ryan C. Thompson <rct@thompsonclan.org>
2013-10-14 17:07:07 -0700

    Add provide & require statements to noop-fetcher

--- lisp/fetchers/el-get-noop-fetcher.el
+++ lisp/fetchers/el-get-noop-fetcher.el
@@ -1,5 +1,9 @@
+(require 'el-get-fetcher-registry)
+
 (el-get-register-fetcher 'noop
   :fetch #'ignore)
 
 (loop for alias in '(no-op null)
       do (el-get-register-fetcher-alias alias 'noop))
+
+(provide 'el-get-noop-fetcher)
